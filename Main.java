79c008b37e521cd4011a62e4faa4c8b54e5888b7
Elena <111504533+Ellena2022@users.noreply.github.com>
2022-08-26 16:50:49 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args){
+        System.out.println("y=(2*x+5*x)/4*x");
+        System.out.println("Алгоритм отсутствует");
+    }
+            }
+
+
